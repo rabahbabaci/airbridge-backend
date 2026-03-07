549f42453a7b2cecd8f612d126547fbbaede7a80
rabah babaci <4rabah@gmail.com>
2026-03-06 22:17:51 -0800

    feat: add API keys for RapidAPI and Google Maps to configuration settings

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -6,6 +6,8 @@ class Settings:
     app_version: str = "0.1.0"
     app_env: str = os.getenv("APP_ENV", "development")
     app_port: int = int(os.getenv("APP_PORT", "8000"))
+    rapidapi_key: str = os.getenv("RAPIDAPI_KEY", "")
+    google_maps_api_key: str = os.getenv("GOOGLE_MAPS_API_KEY", "")
 
 
 settings = Settings()
